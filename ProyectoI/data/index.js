b61a5e3bb25933d1b5768264767101e65c58251d
MariaVaras <mariavarascastex@icloud.com>
2022-09-09 10:45:16 -0300

    trabajando en las vistas generales del punto 1

--- ProyectoI/data/index.js
+++ ProyectoI/data/index.js
@@ -2,7 +2,11 @@ const data = {
 usuarios:[{
    nombre:'rocco', 
    username:'roccoP',
-   foto: '/images/usuarios/messi.jpg'
+   foto: '/images/usuarios/messi.jpg',
+   mail: 'mariavaras@hotmail.com',
+   contrasenia: 'contrasenia12',
+   nacimiento: '2002-11-05',
+   DNI: '44566344',
 }],
 posteos:[{
     foto:'/images/posteos/ronaldo.jpg',
