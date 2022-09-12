dece9bf0f41746f795a3b3f7864e2324f34201cf
Roccopellegrinoo <rpellegrino@udesa.edu.ar>
2022-09-12 12:06:49 -0300

    n

--- ProyectoI/data/index.js
+++ ProyectoI/data/index.js
@@ -1,5 +1,6 @@
 const data = {
 usuarios:[{
+    id: 1,
    nombre:'Roberto', 
    username:'AnteojosRobert',
    foto: '/images/usuarios/usuario_roberto.jpeg',
@@ -9,7 +10,9 @@ usuarios:[{
    DNI: '44566344',
 
 },
-{ nombre:'Beatriz ', 
+{
+    id: 2,
+ nombre:'Beatriz ', 
 username:'La Betty',
 foto: '/images/usuarios/usuario_roberto.jpeg',
 mail: 'Labetty@gmail.com',
@@ -18,7 +21,8 @@ nacimiento: '1995-11-05',
 DNI: '41440334',
 
 }, 
-{nombre:'Pedrito', 
+{id: 3,
+nombre:'Pedrito', 
 username:'El Petter',
 foto: '/images/usuarios/usuario_roberto.jpeg',
 mail: 'PedroVelazco@gmail.com',
@@ -26,7 +30,8 @@ contrasenia: 'contrasenia123',
 nacimiento: '1991-09-006',
 DNI: '40242000',
 },
-{nombre:'Pedrito', 
+{id:4,
+nombre:'Pedrito', 
 username:'El Petter',
 foto: '/images/usuarios/usuario_roberto.jpeg',
 mail: 'PedroVelazco@gmail.com',
@@ -34,7 +39,8 @@ contrasenia: 'contrasenia123',
 nacimiento: '1991-09-06',
 DNI: '40242000',
 },
-{nombre:'Martha', 
+{id: 5,
+nombre:'Martha', 
 username:'MartitaEnMoto',
 foto: '/images/usuarios/usuario_roberto.jpeg',
 mail: 'MarthaLamothe@gmail.com',
@@ -46,15 +52,37 @@ DNI: '434568574',
 
 
 posteos:[{
+    id_posteo : 1,
     foto:'/images/posteos/posteo1.jpeg',
     texto:'Modelo verano',
 },{
+    id_posteo : 1,
     foto:'/images/posteos/posteo2.jpeg',
     texto:'Ultimo modelo '
 },{
+    id_posteo : 1,
     foto:'/images/posteos/posteo3.jpeg',
     texto:'Disfruta del verano',
 }
+
+],
+comentarios : [
+    {id_comment :1,
+        texto: "Buenardos esos lentes"
+    
+    },
+    {id_comment :2,
+        texto: "ke precio tienen esos?"
+    
+    },
+    {id_comment :3,
+        texto: "tenes unos mas masculinos?"
+    
+    },
+    {id_comment :4,
+        texto: "hay en color negros?"
+    
+    },
 
 ]
 }
