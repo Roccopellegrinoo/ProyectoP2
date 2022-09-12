5fac40e8fc5d76511ed4ba50b476e124431b610b
macszim <macszimmermann@gmail.com>
2022-09-12 11:26:06 -0300

    trabajando en el css

--- ProyectoI/data/index.js
+++ ProyectoI/data/index.js
@@ -7,20 +7,58 @@ usuarios:[{
    contrasenia: 'contrasenia12',
    nacimiento: '2002-11-05',
    DNI: '44566344',
-}],
+
+},
+{ nombre:'Beatriz ', 
+username:'La Betty',
+foto: '/images/usuarios/usuario_roberto.jpeg',
+mail: 'Labetty@gmail.com',
+contrasenia: 'contrasenia13',
+nacimiento: '1995-11-05',
+DNI: '41440334',
+
+}, 
+{nombre:'Pedrito', 
+username:'El Petter',
+foto: '/images/usuarios/usuario_roberto.jpeg',
+mail: 'PedroVelazco@gmail.com',
+contrasenia: 'contrasenia123',
+nacimiento: '1991-09-006',
+DNI: '40242000',
+},
+{nombre:'Pedrito', 
+username:'El Petter',
+foto: '/images/usuarios/usuario_roberto.jpeg',
+mail: 'PedroVelazco@gmail.com',
+contrasenia: 'contrasenia123',
+nacimiento: '1991-09-06',
+DNI: '40242000',
+},
+{nombre:'Martha', 
+username:'MartitaEnMoto',
+foto: '/images/usuarios/usuario_roberto.jpeg',
+mail: 'MarthaLamothe@gmail.com',
+contrasenia: 'contrasenia34',
+nacimiento: '1990-10-07',
+DNI: '434568574',
+}
+],
+
+
 posteos:[{
     foto:'/images/posteos/posteo1.jpeg',
-    texto:'antejos de sol modelo verano',
+    texto:'Modelo verano',
 },{
     foto:'/images/posteos/posteo2.jpeg',
-    texto:'anteojos de sol ultimo modelo '
+    texto:'Ultimo modelo '
 },{
     foto:'/images/posteos/posteo3.jpeg',
-    texto:'Disfruta del verano con nuestros anteojos',
+    texto:'Disfruta del verano',
 }
 
 ]
-
 }
+
+
 
 module.exports = data
