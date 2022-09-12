b005f982f96c018241e3ff140c97dcf1190080a3
Roccopellegrinoo <rpellegrino@udesa.edu.ar>
2022-09-12 09:38:29 -0300

    n

--- ProyectoI/data/index.js
+++ ProyectoI/data/index.js
@@ -1,20 +1,24 @@
 const data = {
 usuarios:[{
-   nombre:'rocco', 
-   username:'roccoP',
-   foto: '/images/usuarios/messi.jpg',
-   mail: 'mariavaras@hotmail.com',
+   nombre:'Roberto', 
+   username:'AnteojosRobert',
+   foto: '/images/usuarios/usuario_roberto.jpeg',
+   mail: 'roberto@gmail.com',
    contrasenia: 'contrasenia12',
    nacimiento: '2002-11-05',
    DNI: '44566344',
 }],
 posteos:[{
-    foto:'/images/posteos/ronaldo.jpg',
-    texto:'cr7',
+    foto:'/images/posteos/posteo1.jpeg',
+    texto:'antejos de sol modelo verano',
 },{
-    foto:'/images/posteos/ronaldo.jpg',
-    texto:'cr7'
+    foto:'/images/posteos/posteo2.jpeg',
+    texto:'anteojos de sol ultimo modelo '
+},{
+    foto:'/images/posteos/posteo3.jpeg',
+    texto:'Disfruta del verano con nuestros anteojos',
 }
+
 ]
 
 }
