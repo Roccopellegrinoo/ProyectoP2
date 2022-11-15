66c960e9469990834057df29481140cbcd7247ed
macszim <malfonso@udesa.edu.ar>
2022-11-15 14:10:03 -0300

    trabajando en index controller

--- ProyectoI/controllers/indexController.js
+++ ProyectoI/controllers/indexController.js
@@ -1,9 +1,27 @@
 const data = require('../data/index')
+const db = require('../database/models')
+const op = db.Sequelize.Op;
 const indexController = {
-home: function(req, res) {
-
-    res.render('index', { info:data.usuarios, data: data.posteos });
-  },
+home: function(req,res){
+db.Posteo.findAll({
+  include:[
+    {
+      model:db.Usuario,
+      as:'comentarios_post',
+      through:{
+        attributes:["texto"],
+      }
+    },
+    {
+      model:db.Usuario,
+      as:'usuario'
+    }
+  ]
+})
+.then((posteos)=>{
+  res.render('index',{posteos: posteos});
+})
+},
 
 search: function(req, res) {
   let texto = req.query.texto;
